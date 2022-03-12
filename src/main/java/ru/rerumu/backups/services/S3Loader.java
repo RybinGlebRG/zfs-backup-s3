85af209d7faeff95e0fb00e25169c734513c3fb7
rybin.gleb <32393520+RybinGlebRG@users.noreply.github.com>
2022-03-13 01:15:00 +0300

    Adding multipart upload

--- src/main/java/ru/rerumu/backups/services/S3Loader.java
+++ src/main/java/ru/rerumu/backups/services/S3Loader.java
@@ -4,15 +4,24 @@ import ru.rerumu.backups.models.S3Storage;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.UploadPartRequest;
+import software.amazon.awssdk.services.s3.model.UploadPartResponse;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class S3Loader {
 
+    private static final Long PART_SIZE = 104857600L; // 100MB
     private final List<S3Storage> storages = new ArrayList<>();
 
     public void addStorage(S3Storage s3Storage){
@@ -22,11 +31,7 @@ public class S3Loader {
     public void upload(Path path){
         // TODO: Check we have storages added
         for (S3Storage item:storages ) {
-            S3Client s3 = S3Client.builder()
-                    .credentialsProvider(StaticCredentialsProvider.create(item.getCredentials()))
-                    .region(item.getRegion())
-                    .endpointOverride(item.getEndpoint())
-                    .build();
+            S3Client s3 = prepareS3Client(item);
             String key = item.getPrefix().toString()+"/"+path.getFileName().toString();
             PutObjectRequest objectRequest = PutObjectRequest.builder()
                     .bucket(item.getBucketName())
@@ -37,5 +42,53 @@ public class S3Loader {
 
         }
 
+    }
+
+    private S3Client prepareS3Client(S3Storage s3Storage){
+        S3Client s3 = S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(s3Storage.getCredentials()))
+                .region(s3Storage.getRegion())
+                .endpointOverride(s3Storage.getEndpoint())
+                .build();
+        return s3;
+    }
+
+    public void multipartUpload(Path path) throws IOException{
+        // TODO: Check we have storages added
+        for (S3Storage item:storages ) {
+            S3Client s3 = prepareS3Client(item);
+            String key = item.getPrefix().toString()+"/"+path.getFileName().toString();
+            var multipartUploadRequest = CreateMultipartUploadRequest.builder()
+                    .bucket(item.getBucketName())
+                    .key(key)
+                    .storageClass(item.getStorageClass())
+                    .build();
+            var multipartUpload = s3.createMultipartUpload(multipartUploadRequest);
+            String uploadId = multipartUpload.uploadId();
+            int partNumber = 0;
+            String abortRuleId = multipartUpload.abortRuleId();
+
+            // TODO: Limit one part by size
+            try(BufferedInputStream bufferedInputStream = new BufferedInputStream(Files.newInputStream(path))){
+                Path partPath = Paths.get(path.getParent().toString(),UUID.randomUUID().toString());
+                try(BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(Files.newOutputStream(partPath))){
+                    byte[] buf = new byte[1024];
+                    int len;
+                    while ((len = bufferedInputStream.read(buf)) >= 0) {
+                        bufferedOutputStream.write(buf, 0, len);
+                    }
+                }
+                UploadPartRequest uploadPartRequest = UploadPartRequest.builder()
+                        .partNumber(partNumber)
+                        .uploadId(uploadId)
+                        .bucket(item.getBucketName())
+                        .key(key)
+                        .build();
+                UploadPartResponse uploadPartResponse = s3.uploadPart(uploadPartRequest,partPath);
+                // TODO: Wait until loading is finished
+            }
+
+
+        }
     }
 }
