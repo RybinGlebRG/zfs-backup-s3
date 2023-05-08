bf3ced9ff18ffbe2a14120f83e8dcb8af5f24b40
rybin.gleb <32393520+RybinGlebRG@users.noreply.github.com>
2023-05-08 13:40:49 +0300

    New test

---
+++ zfs/src/test/java/ru/rerumu/zfs/consumers/TestSnapshotListStdConsumer.java
@@ -0,0 +1,56 @@
+package ru.rerumu.zfs.consumers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.rerumu.zfs.models.Snapshot;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestSnapshotListStdConsumer {
+
+    @Test
+    void shouldCall(){
+        List<Snapshot> snapshotList = new ArrayList<>();
+        String str = """
+                Test@tmp1
+                Test@tmp2
+                Test@tmp3
+                """;
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(byteArrayInputStream);
+
+
+        SnapshotListStdConsumer snapshotListStdConsumer = new SnapshotListStdConsumer(snapshotList);
+        snapshotListStdConsumer.accept(bufferedInputStream);
+
+
+        List<Snapshot> expectedSnapshotList = new ArrayList<>();
+        expectedSnapshotList.add(new Snapshot("Test@tmp1"));
+        expectedSnapshotList.add(new Snapshot("Test@tmp2"));
+        expectedSnapshotList.add(new Snapshot("Test@tmp3"));
+
+        Assertions.assertEquals(expectedSnapshotList,snapshotList);
+    }
+
+    @Test
+    void shouldThrowException() throws Exception{
+        List<Snapshot> snapshotList = new ArrayList<>();
+        BufferedInputStream bufferedInputStream = mock(BufferedInputStream.class);
+
+        when(bufferedInputStream.readAllBytes()).thenThrow(IOException.class);
+
+
+        SnapshotListStdConsumer snapshotListStdConsumer = new SnapshotListStdConsumer(snapshotList);
+        Assertions.assertThrows(RuntimeException.class,()->snapshotListStdConsumer.accept(bufferedInputStream));
+
+    }
+}
