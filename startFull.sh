1e5b08042976435498bd2128588eeec9dc42e25e
rybin.gleb <32393520+RybinGlebRG@users.noreply.github.com>
2023-06-21 21:04:44 +0300

    Altered configuration

--- startFull.sh
+++ startFull.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+export ZFS_BACKUP_S3_REGION=***
+export ZFS_BACKUP_S3_BUCKET=***
+export ZFS_BACKUP_S3_ACCESS_KEY_ID=***
+export ZFS_BACKUP_S3_SECRET_ACCESS_KEY=***
+export ZFS_BACKUP_S3_FULL_PREFIX=***
+export ZFS_BACKUP_S3_FULL_STORAGE_CLASS=***
+export ZFS_BACKUP_S3_MAX_S3_PART_SIZE=***
+export ZFS_BACKUP_S3_MAX_FILE_SIZE=***
+export ZFS_BACKUP_S3_TEMP_DIR=***
+
 nohup java -Xms1g -Xmx3g \
 -XX:+HeapDumpOnOutOfMemoryError \
 -Dcom.sun.management.jmxremote \
